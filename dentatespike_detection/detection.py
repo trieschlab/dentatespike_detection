fda552e08a5546bf0348de191f10689a2dace31f
tristan <tristan.stoeber@posteo.net>
2021-03-05 16:05:38 +0100

    vae with dense/dropout encoder/decoder

--- dentatespike_detection/detection.py
+++ dentatespike_detection/detection.py
@@ -2,10 +2,16 @@ from scipy.stats import zscore
 import pandas as pd
 import ripple_detection as rd
 import numpy as np
+import pdb
+import pickle
+import os
+import csv
 
-def detect_dentate_spikes(
+
+def detect_dentate_spikes_nokia2017(
     data,
-    time,                                                                                                   thres_dur,
+    time,
+    thres_dur,
     thres_zscore,
     thres_peak,
     thres_rise,
@@ -27,14 +33,15 @@ def detect_dentate_spikes(
     thres_peak : float, minimal peak value in mV,
     thres_rise : float, minimal difference to peak in preceding window mV,
     wndw_rise : array_like, shape (2,), start and end of preceding window in s,
-    polarity : signed int, 1 or -1, wether ds are positive or negative deflections,
+    polarity : signed int, 1 or -1, wether ds are positive or negative
+               deflections,
     center : bool, wheter or not mean is subtracted,
     return_trace : bool, wether LFP trace is returned,
     wdnw_trace : array_like, shape (2,), start and end of trace in s,
     
     Returns
     -------
-    df : pd.DataFrame, results 
+    df : pd.DataFrame, results
     """
     # adjust for polarity
     data = data*polarity
@@ -51,12 +58,12 @@ def detect_dentate_spikes(
     evts = rd.core.segment_boolean_series(
         is_above_threshold, minimum_duration=thres_dur)
 
-    # determine position of start and stop times in data                                                                                                                                                           
+    # determine position of start and stop times in data
     tps = np.array(evts).flatten()
     pos_tps = np.searchsorted(time, tps)
     pos_tps = list(zip(pos_tps[::2],pos_tps[1::2]))
 
-    # get peak times and amplitudes                                                                                                                                                                                
+    # get peak times and amplitudes
     peaks_amp = []
     peaks_zscore = []
     peaks_t = []
@@ -67,15 +74,18 @@ def detect_dentate_spikes(
         peak_i = data[pos_start+peak_pos_i]
         peak_zscore_i = zscored_data[pos_start+peak_pos_i]
 
-        peaks_amp.append(peak_i)
-        peaks_t.append(peak_t_i)
-        peaks_zscore.append(peak_zscore_i)
+        # ignore events which are too close to start
+        # start of preceding window must be within recording period
+        if peak_t_i + wndw_rise[0] >= np.min(time):
+            peaks_amp.append(peak_i)
+            peaks_t.append(peak_t_i)
+            peaks_zscore.append(peak_zscore_i)
     
     peaks_amp = np.array(peaks_amp)
     peaks_zscore = np.array(peaks_zscore)
     peaks_t = np.array(peaks_t)
     
-    # get maximum value in time window before the spike                                                                                                                                                                                       
+    # get maximum value in time window before the spike
     ts_wndw_start = peaks_t+wndw_rise[0]
     ts_wndw_stop = peaks_t+wndw_rise[1]
     ts_wndw_both = np.sort(np.concatenate((ts_wndw_start, ts_wndw_stop)))
@@ -124,9 +134,10 @@ def detect_dentate_spikes(
     
     # return traces if desired
     if return_trace:
-        ts_trace_start = np.array(peaks_t) - wdnw_trace[0]
-        ts_trace_stop = np.array(peaks_t) - wdnw_trace[1]
-        ts_trace_both = np.sort(np.concatenate((ts_trace_start, ts_trace_stop)))
+        ts_trace_start = np.array(peaks_t) + wdnw_trace[0]
+        ts_trace_stop = np.array(peaks_t) + wdnw_trace[1]
+        ts_trace_both = np.sort(
+            np.concatenate((ts_trace_start, ts_trace_stop)))
         pos_trace = np.searchsorted(time, ts_trace_both)
         pos_trace = list(zip(pos_trace[::2], pos_trace[1::2]))
         
@@ -149,3 +160,198 @@ def detect_dentate_spikes(
         df = df.join(df_trace)
         #df = pd.concat([df, df_trace], keys=list(df.keys())+list(df_trace.keys()), axis=1, ignore_index=True)
     return df
+
+
+def detect_dentate_spikes_zscore(
+    data,
+    time,
+    thres_dur,
+    thres_zscore_peak,
+    thres_zscore_rise,
+    wndw_rise,
+    polarity=1,
+    center=True,
+    return_only_valid=True,
+    return_trace=False,
+    wdnw_trace=[-0.1, 0.1],
+):
+    
+    """
+    Params
+    ------
+    data : array_like, shape (n_time,),
+    time : array_like, shape (n_time,),
+    thres_zscore, float, minimal zscore,
+    thres_dur : float, minimal duration of zscore crossing in s,
+    thres_peak : float, minimal peak value in mV,
+    thres_rise : float, minimal difference to peak in preceding window mV,
+    wndw_rise : array_like, shape (2,), start and end of preceding window in s,
+    polarity : signed int, 1 or -1, wether ds are positive or negative
+               deflections,
+    center : bool, wheter or not mean is subtracted,
+    return_trace : bool, wether LFP trace is returned,
+    wdnw_trace : array_like, shape (2,), start and end of trace in s,
+    
+    Returns
+    -------
+    df : pd.DataFrame, results
+    """
+    # adjust for polarity
+    data = data*polarity
+    
+    if center:
+        data -= np.mean(data)
+
+#    evts = rd.core.threshold_by_zscore(data, time, min_duration, zscore_thres)
+    # from github Eden-Kramer-Lab/ripple_detection/.core threshold_by_zscore
+    zscored_data = zscore(data)
+    is_above_threshold = zscored_data > thres_zscore_peak
+    is_above_threshold = pd.Series(is_above_threshold, index=time)
+    
+    evts = rd.core.segment_boolean_series(
+        is_above_threshold, minimum_duration=thres_dur)
+
+    # determine position of start and stop times in data
+    tps = np.array(evts).flatten()
+    pos_tps = np.searchsorted(time, tps)
+    pos_tps = list(zip(pos_tps[::2], pos_tps[1::2]))
+
+    # get peak times and amplitudes
+    peaks_amp = []
+    peaks_zscore = []
+    peaks_t = []
+    for pos_start, pos_stop in pos_tps:
+        data_i = data[pos_start:pos_stop]
+        peak_pos_i = np.argmax(data_i)
+        peak_t_i = time[pos_start+peak_pos_i]
+        peak_i = data[pos_start+peak_pos_i]
+        peak_zscore_i = zscored_data[pos_start+peak_pos_i]
+
+        # ignore events which are too close to start
+        # start of preceding window must be within recording period
+        if peak_t_i + wndw_rise[0] >= np.min(time):
+            peaks_amp.append(peak_i)
+            peaks_t.append(peak_t_i)
+            peaks_zscore.append(peak_zscore_i)
+    
+    peaks_amp = np.array(peaks_amp)
+    peaks_zscore = np.array(peaks_zscore)
+    peaks_t = np.array(peaks_t)
+    
+    # get maximum value in time window before the spike
+    ts_wndw_start = peaks_t+wndw_rise[0]
+    ts_wndw_stop = peaks_t+wndw_rise[1]
+    ts_wndw_both = np.sort(np.concatenate((ts_wndw_start, ts_wndw_stop)))
+    pos_tps_rise = np.searchsorted(time, ts_wndw_both)
+    pos_tps_rise = list(zip(pos_tps_rise[::2], pos_tps_rise[1::2]))
+
+    peaks_zscore_wndw_bfr = []
+    t_peak_wndw_bfr = []
+    
+    for i in range(len(peaks_amp)):
+        pos_start = pos_tps_rise[i][0]
+        pos_stop = pos_tps_rise[i][1]
+        data_i = zscored_data[pos_start:pos_stop]
+        peak_pos_i = np.argmax(data_i)
+        peak_i = data_i[peak_pos_i]
+        peak_t_i = time[pos_start+peak_pos_i]
+        
+        peaks_zscore_wndw_bfr.append(peak_i)
+        t_peak_wndw_bfr.append(peak_t_i)
+
+    peaks_zscore_wndw_bfr = np.array(peaks_zscore_wndw_bfr)
+    t_peak_wndw_bfr = np.array(t_peak_wndw_bfr)
+    
+    # evaluate rise condition
+    bool_rise = (peaks_zscore-peaks_zscore_wndw_bfr) > thres_zscore_rise
+
+    peaks_amp = peaks_amp[bool_rise]
+    peaks_zscore = peaks_zscore[bool_rise]
+    peaks_t = peaks_t[bool_rise]
+    peaks_zscore_wndw_bfr = peaks_zscore_wndw_bfr[bool_rise]
+    t_peak_wndw_bfr = t_peak_wndw_bfr[bool_rise]
+    
+    evnts_dict = {
+        'peaks_amp': peaks_amp,
+        'peaks_zscore': peaks_zscore,
+        'peaks_t': peaks_t,
+        'peaks_zscore_wndw_bfr': peaks_zscore_wndw_bfr,
+        't_peaks_zscore_wndw_bfr': t_peak_wndw_bfr,
+    }
+
+    df = pd.DataFrame.from_dict(evnts_dict)
+
+    
+    # return traces if desired
+    if return_trace:
+        ts_trace_start = np.array(peaks_t) + wdnw_trace[0]
+        ts_trace_stop = np.array(peaks_t) + wdnw_trace[1]
+        ts_trace_both = np.sort(
+            np.concatenate((ts_trace_start, ts_trace_stop)))
+        pos_trace = np.searchsorted(time, ts_trace_both)
+        pos_trace = list(zip(pos_trace[::2], pos_trace[1::2]))
+        
+        lst_trace = []
+        lst_t_trace = []
+        lst_zscore = []
+        for pos_start_i, pos_stop_i in pos_trace:
+            lst_trace.append(data[pos_start_i:pos_stop_i])
+            lst_t_trace.append(time[pos_start_i:pos_stop_i])
+            lst_zscore.append(zscored_data[pos_start_i:pos_stop_i])
+        
+        #df['trace'] = []
+        #evnts_dict['traces'] = lst_trace
+        df_trace = pd.DataFrame({'trace': [], 't_trace': [], 'trace_zscore': []})
+        df_trace.astype(object)
+        df_trace['trace'] = lst_trace
+        df_trace['t_trace'] = lst_t_trace
+        df_trace['trace_zscore'] = lst_zscore
+        #pdb.set_trace()
+        df = df.join(df_trace)
+        #df = pd.concat([df, df_trace], keys=list(df.keys())+list(df_trace.keys()), axis=1, ignore_index=True)
+    return df
+
+
+def create_folder_structure(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(path + ' created')
+
+def save_dict(di_, filename_):
+    with open(filename_, 'wb') as f:
+        pickle.dump(di_, f)
+
+def load_dict(filename_):
+    with open(filename_, 'rb') as f:
+        ret_di = pickle.load(f)
+    return ret_di
+
+
+def get_result(result):
+    global results
+    results.append(result)
+    
+
+def load_detect_store(
+        fun_dect, fname_in, fname_out, t_start, sampling_int, p_det):
+    """ Wrapper function for multiprocessing
+    Load data, detect dentate spikes and store result
+    """
+    try:
+        # open file        
+        with open(fname_in, 'r') as f:
+            reader = csv.reader(f, delimiter=',')
+            data = np.array(list(reader), dtype=np.float).flatten()
+        # convert to mV
+        data = data/1000.
+
+        t = np.arange(t_start, t_start+len(data)*sampling_int, sampling_int)
+
+        df_ds = fun_dect(
+            data, t,
+            **p_det)
+
+        save_dict(df_ds, fname_out)
+    
+    except Exception as e:
+        print(e)
--- dentatespike_detection/vae.py
+++ dentatespike_detection/vae.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import numpy as np
 
 
 class Sampling(layers.Layer):
@@ -15,10 +16,182 @@ class Sampling(layers.Layer):
 
 
 class VAE(keras.Model):
+    def __init__(self,
+                 encoder,
+                 decoder,
+                 kl_ratio,
+                 **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.kl_ratio = kl_ratio
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+        
+    def train_step(self, data):
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+            mse = keras.losses.MeanSquaredError()
+            reconstruction_loss = mse(data, reconstruction)
+            kl_loss = self.kl_ratio*-0.5 * (1 + z_log_var - tf.square(z_mean)
+                                            - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            total_loss = reconstruction_loss + kl_loss
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+    
+
+def build_encoder_small(
+        enc_units=[512, 512, 256, 128, 2],
+        drop_fc=0.5, actfn='relu'):
+    """
+    Build simple autoencoder with only dense and dropout layers
+    
+    Params
+    ------
+    enc_units : list
+       Number of units per layer. First/last layer must match input/output.
+    drop_fc : float
+       Fraction of dropout units
+    actfn : str ['relu'], keras activation function
+
+    Returns
+    -------
+    encoder : keras.Model
+
+    """
+
+    enc_units = np.array(enc_units)
+    
+    # define input layer
+    encoder_inputs = layers.Input(shape=(enc_units[0],))
+    x = encoder_inputs
+    for n_i in enc_units[1:-2]:
+        x = layers.Dense(n_i, activation=actfn)(x)
+        x = layers.Dropout(drop_fc)(x)
+    x = layers.Flatten()(x)
+    
+    z_mean = layers.Dense(enc_units[-1], name="z_mean")(x)
+    z_log_var = layers.Dense(enc_units[-1], name="z_log_var")(x)
+    z = Sampling()([z_mean, z_log_var])
+    encoder = keras.Model(
+        encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+    encoder.summary()
+    return encoder
+
+    
+def build_decoder_small(
+        dec_units=[2, 128, 256, 512, 512],
+        drop_fc=0.5, actfn='relu'):
+    """
+    Build simple autoencoder with only dense and dropout layers
+    
+    Params
+    ------
+    dec_units : list
+       Number of units per layer. First/last layer must match input/output.
+    drop_fc : float
+       Fraction of dropout units
+    actfn : str ['relu'], keras activation function
+
+    Returns
+    -------
+    encoder : keras.Model
+
+    """
+    dec_units = np.array(dec_units)
+    
+    latent_inputs = keras.Input(shape=(dec_units[0],))
+    x = latent_inputs
+    
+    for n_i in dec_units[1:-2]:
+        x = layers.Dense(n_i, activation=actfn)(x)
+        x = layers.Dropout(drop_fc)(x)
+    x = layers.Flatten()(x)
+    
+    decoder_outputs = layers.Dense(dec_units[-1])(x)
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    decoder.summary()
+    return decoder
+
+    
+def build_encoder_convolutional(self, latent_dim, X_shape):
+    """
+    Build the encoder
+    """
+    encoder_inputs = layers.Input(shape=(X_shape[1], X_shape[2]))
+    x = encoder_inputs
+    x = layers.Conv1D(
+        filters=8, kernel_size=7, padding="same", strides=2, activation="relu"
+    )(x)
+    x = layers.Conv1D(
+        filters=4, kernel_size=7, padding="same", strides=4, activation="relu"
+    )(x)
+    x = layers.Conv1D(
+        filters=2, kernel_size=7, padding="same", strides=4, activation="relu"
+    )(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(16,)(x)
+    z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    z = Sampling()([z_mean, z_log_var])
+    encoder = keras.Model(
+        encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+    encoder.summary()
+    return encoder
+
+
+def build_decoder_convolutional(self, latent_dim, X_shape):
+    """
+    Build the decoder
+    """
+    latent_inputs = keras.Input(shape=(latent_dim,))
+    x = latent_inputs
+    x = layers.Dense(16)(x)
+    x = layers.Reshape((8, 2))(x)
+    x = layers.Conv1DTranspose(
+        filters=4, kernel_size=7, padding="same",
+        strides=4, activation="relu"
+    )(x)
+    x = layers.Conv1DTranspose(
+        filters=8, kernel_size=7, padding="same",
+        strides=4, activation="relu"
+    )(x)
+    x = layers.Conv1DTranspose(
+        filters=1, kernel_size=7, padding="same",
+        strides=2, activation="relu"
+    )(x)
+    decoder_outputs = layers.Dense(16)(x)
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    decoder.summary()
+    return decoder
+
+    
+class VAE_old(keras.Model):
     def __init__(
             self, latent_dim, X_shape, **kwargs):
         latent_dim = 2
-        super(VAE, self).__init__(**kwargs)
+        super(VAE_old, self).__init__(**kwargs)
         self.encoder = self.build_encoder(latent_dim, X_shape)
         self.decoder = self.build_decoder(latent_dim, X_shape)
         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
